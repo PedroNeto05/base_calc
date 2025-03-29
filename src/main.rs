41a34858b347f17059b8366a8d01fbc01a033415
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-03-29 11:50:58 -0300

    test case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+mod conversion;
 mod utils;
 
 fn main() {
-    let teste = utils::input("Digite algo: ");
+    let number_in_b1 = utils::input("Digite um numero em uma base b1: ");
+    let base_to = utils::input("Digite uma base para transformar o numero: ").parse::<u8>().unwrap();
+    let f = conversion::FloatPoint {
+        base_to,
+        mantissa: 3,
+        min_exp: 1,
+        max_exp: 1,
+        base_from: 10,
+    };
+    let number_in_b10 = conversion::to_base_10(&number_in_b1, f);
+    println!("numero na b10: {number_in_b10}")
 }
