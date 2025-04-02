480802ef2f326ce5c1824c1f38b9c5af47d02520
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-04-02 07:24:14 -0300

    remove float struct

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,11 +1,3 @@
-pub struct FloatPoint {
-    pub base_from: u8,
-    pub base_to: u8,
-    pub mantissa: u64,
-    pub min_exp: i32,
-    pub max_exp: i32,
-}
-
 fn char_to_digit(digit: &char) -> Option<u8> {
     match digit {
         '0'..='9' => Some(*digit as u8 - b'0'),
@@ -27,21 +19,21 @@ fn to_base_10_int_part(int_part: String, base: u8) -> f64 {
     sum as f64
 }
 
-fn to_base_10_float_part(float_part: String, f: FloatPoint) -> f64 {
+fn to_base_10_float_part(float_part: String, base_to: u8) -> f64 {
     let mut sum: f64 = 0.0;
     let digits: Vec<char> = float_part.chars().collect();
 
     for (i, digit) in digits.iter().enumerate() {
         let digit_value = char_to_digit(digit).unwrap();
         let exp: i32 = -(i as i32 + 1);
-        sum += digit_value as f64 * (f.base_to as f64).powi(exp);
+        sum += digit_value as f64 * (base_to as f64).powi(exp);
     }
 
     // 0.123
     sum
 }
 
-pub fn to_base_10(num: &str, f: FloatPoint) -> String {
+pub fn to_base_10(num: &str, base_to: u8) -> String {
     let parts: Vec<&str> = num.split('.').collect();
     let int_part = parts[0];
     let float_part = if parts.len() > 1 { parts[1] } else { "" };
@@ -49,8 +41,8 @@ pub fn to_base_10(num: &str, f: FloatPoint) -> String {
     let int_part = int_part.to_string();
     let float_part = float_part.to_string();
 
-    let int_part = to_base_10_int_part(int_part, f.base_to);
-    let float_part = to_base_10_float_part(float_part, f);
+    let int_part = to_base_10_int_part(int_part, base_to);
+    let float_part = to_base_10_float_part(float_part, base_to);
 
     format!("{}", int_part + float_part)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,8 @@ mod conversion;
 mod utils;
 
 fn main() {
-    let number_in_b1 = utils::input("Digite um numero em uma base b1: ");
     let base_to = utils::input("Digite uma base para transformar o numero: ").parse::<u8>().unwrap();
-    let f = conversion::FloatPoint {
-        base_to,
-        mantissa: 3,
-        min_exp: 1,
-        max_exp: 1,
-        base_from: 10,
-    };
-    let number_in_b10 = conversion::to_base_10(&number_in_b1, f);
+    let number_in_b1 = utils::input("Digite um numero em uma base b1: ");
+    let number_in_b10 = conversion::to_base_10(&number_in_b1, base_to);
     println!("numero na b10: {number_in_b10}")
 }
