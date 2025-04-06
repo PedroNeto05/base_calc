706d8bcc35853b9bb5d72a5b7581d9e46441621c
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-04-06 09:37:38 -0300

    sum to any base

--- src/bases/operations.rs
+++ src/bases/operations.rs
@@ -1,9 +1,11 @@
 use super::conversion;
 
-pub fn sum(num1:&str,base1:u8,num2:&str,base2:u8) -> f64 {
-   return conversion::to_base_10(num1,base1) + conversion::to_base_10(num2,base2); 
+pub fn sum(num1: &str, base1: u8, num2: &str, base2: u8, base_dest: u8) -> String {
+    let result = conversion::to_base_10(num1, base1) + conversion::to_base_10(num2, base2);
+    conversion::euclidean(result, base_dest)
 }
 
-pub fn prod(num1:&str,base1:u8,num2:&str,base2:u8) -> f64 {
-   return conversion::to_base_10(num1,base1) * conversion::to_base_10(num2,base2); 
+pub fn prod(num1: &str, base1: u8, num2: &str, base2: u8, base_dest: u8) -> String {
+    let result = conversion::to_base_10(num1, base1) * conversion::to_base_10(num2, base2);
+    conversion::euclidean(result, base_dest)
 }
