eaac6d11e11b6db12a7c4989a50a757f6dced67e
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-04-05 21:16:25 -0300

    change order

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod utils;
 mod bases;
 
 fn main() {
-    let base_to = utils::input("Digite uma base para transformar o numero: ").parse::<u8>().unwrap();
-    let number_in_b1 = utils::input("Digite um numero em uma base b1: ");
+    let number_in_b1 = utils::input("Digite um numero para ser transformado para a b10: ");
+    let base_to = utils::input("Digite sua base: ").parse::<u8>().unwrap();
     let number_in_b10 = bases::conversion::to_base_10(&number_in_b1, base_to);
     println!("numero na b10: {number_in_b10}");
 
